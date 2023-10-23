9a48930fb85968fd9fe36c9ae636cf8cb2e77503
jingyinggggg <133077421+jingyinggggg@users.noreply.github.com>
2023-10-23 19:18:29 +0800

    Sign Up Page - Completed
    - Done database connection

--- app/src/main/java/com/example/transportpro/SignUpPage.java
+++ app/src/main/java/com/example/transportpro/SignUpPage.java
@@ -1,91 +1,157 @@
 package com.example.transportpro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.transportpro.databinding.SignUpPageBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SignUpPage extends AppCompatActivity {
 
-    Button signUp;
+    SignUpPageBinding binding;
+    String fullname, username, email, contact, password, Rpassword;
+    FirebaseDatabase db;
+    DatabaseReference reference;
 
-    //Dialog Variables
+    // Dialog Variables
     Dialog showDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up_page);
+        binding = SignUpPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        signUp = findViewById(R.id.signUpButton);
-        signUp.setOnClickListener(new View.OnClickListener() {
+        binding.signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAlertDialog(R.layout.sign_up_successful, 1);
-//                showAlertDialog(R.layout.sign_up_fail, 2);
+                fullname = binding.nameInput.getText().toString();
+                username = binding.usernameInput.getText().toString().toLowerCase(Locale.ROOT);
+                email = binding.emailInput.getText().toString();
+                contact = binding.phoneInput.getText().toString();
+                password = binding.passwordInput.getText().toString();
+                Rpassword = binding.reenterPasswordInput.getText().toString();
+
+                if (!fullname.isEmpty() && !username.isEmpty() && !email.isEmpty() && !contact.isEmpty() && !password.isEmpty() && !Rpassword.isEmpty() && password.equals(Rpassword)) {
+                    db = FirebaseDatabase.getInstance();
+                    reference = db.getReference("User");
+
+                    // Query to find the maximum user ID in the database
+                    reference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.exists()){
+                                showAlertDialog(R.layout.sign_up_fail, 2);
+                                Toast.makeText(SignUpPage.this, "Username is already used, please try another", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                // Username is unique, proceed to create the user
+                                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        // Count the number of users with the same username
+                                        int numUsersWithSameUsername = (int) dataSnapshot.getChildrenCount();
+
+                                        // Increment the maximum user ID to generate a new user ID
+                                        int newUserId = numUsersWithSameUsername + 1;
+
+                                        int isAdminAccount = 0;
+                                        int isDeletedAccount = 0;
+
+                                        // Create the User object with the new user ID
+                                        User user = new User(isDeletedAccount, isAdminAccount, newUserId, fullname, username, email, contact, password);
+
+                                        // Show the success dialog
+                                        showAlertDialog(R.layout.sign_up_successful, 1);
+
+                                        // Set the new user data in the database
+                                        setUserInDatabase(username, user);
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+                                        // Handle database error
+                                    }
+                                });
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            // Handle database error
+                        }
+                    });
+                } else {
+                    showAlertDialog(R.layout.sign_up_fail, 2);
+                }
             }
         });
 
-        //Password Validation
-        //Get reference to the TextInputLayout and TextInputEditText for password
+        // Password Validation
+        // Get reference to the TextInputLayout and TextInputEditText for password
         TextInputLayout layoutPassword = findViewById(R.id.textInputLayoutPassword);
         TextInputEditText eTextPassword = findViewById(R.id.passwordInput);
         eTextPassword.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String password = s.toString();
-                if(password.length() >= 8){
+                if (password.length() >= 8) {
                     Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
                     Matcher matcher = pattern.matcher(password);
                     boolean isPwdContainSpeChar = matcher.find();
                     layoutPassword.setHelperText("");
-                    if(isPwdContainSpeChar){
+                    if (isPwdContainSpeChar) {
                         layoutPassword.setError("");
-                    }else{
-                        layoutPassword.setError("Weak Password. Please include minimum 1 special character");
+                    } else {
+                        layoutPassword.setError("Weak Password. Please include a minimum of 1 special character");
                     }
-
-                }else{
-                    layoutPassword.setHelperText("***Password must be minimum 8 character");
+                } else {
+                    layoutPassword.setHelperText("***Password must be a minimum of 8 characters");
                     layoutPassword.setError("");
                 }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-
             }
         });
 
-        //Validate Re-enter password
-        //Get reference to the TextInputLayout and TextInputEditText for reenter password
+        // Validate Re-enter password
+        // Get reference to the TextInputLayout and TextInputEditText for reenter password
         TextInputLayout textInputLayoutReenterPassword = findViewById(R.id.textInputLayoutRepeatPassword);
         TextInputEditText reenterPasswordInput = findViewById(R.id.reenterPasswordInput);
 
         reenterPasswordInput.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
@@ -93,11 +159,11 @@ public class SignUpPage extends AppCompatActivity {
                 String password = eTextPassword.getText().toString();
                 String reenterPassword = reenterPasswordInput.getText().toString();
 
-                //If password does not match reenter password, error message displayed
-                if(password.equals(reenterPassword)){
+                // If password does not match reenter password, error message displayed
+                if (password.equals(reenterPassword)) {
                     textInputLayoutReenterPassword.setError(null);
-                } else{
-                    textInputLayoutReenterPassword.setError("Password do not match");
+                } else {
+                    textInputLayoutReenterPassword.setError("Password does not match");
                 }
             }
 
@@ -105,25 +171,22 @@ public class SignUpPage extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
             }
         });
-
-
     }
 
-    public void backLoginPage(View view){
+    public void backLoginPage(View view) {
         Intent loginPage = new Intent(this, LoginPage.class);
         startActivity(loginPage);
     }
 
-    private void showAlertDialog(int myLayout, int type){
+    private void showAlertDialog(int myLayout, int type) {
         showDialog = new Dialog(SignUpPage.this);
         View layoutView = getLayoutInflater().inflate(myLayout, null);
 
         showDialog.setContentView(layoutView);
         showDialog.setCancelable(false);
         showDialog.getWindow().getAttributes().windowAnimations = R.style.animation;
-
-        //Sign up successful
-        if(type == 1){
+        // Sign up successful
+        if (type == 1) {
             showDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.round_button_success));
 
             Button done = showDialog.findViewById(R.id.done);
@@ -134,29 +197,44 @@ public class SignUpPage extends AppCompatActivity {
                     backLoginPage(v);
                 }
             });
-        }
-        //Sign up fail
-        else{
+        } else {
             showDialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.round_button_fail));
             Button signUpFail = layoutView.findViewById(R.id.tryAgainButton);
-            //Click on try again button
+            // Click on the try again button
             signUpFail.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     showDialog.dismiss();
                 }
             });
-
-            Button backHome = layoutView.findViewById(R.id.backHome);
-            backHome.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    backLoginPage(v);
-                }
-            });
         }
 
         showDialog.show();
+    }
 
+    // Separate method to set user data in the database
+    private void setUserInDatabase(String username, User user) {
+        reference.child(username).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    // Data was successfully written to the database
+                    Log.d("Firebase", "Data write successful");
+                    // Reset the input fields
+                    binding.nameInput.setText("");
+                    binding.usernameInput.setText("");
+                    binding.emailInput.setText("");
+                    binding.phoneInput.setText("");
+                    binding.passwordInput.setText("");
+                } else {
+                    // Handle the error
+                    Exception e = task.getException();
+                    if (e != null) {
+                        Log.e("Firebase", "Data write failed: " + e.getMessage());
+                        showAlertDialog(R.layout.sign_up_fail, 2);
+                    }
+                }
+            }
+        });
     }
 }
